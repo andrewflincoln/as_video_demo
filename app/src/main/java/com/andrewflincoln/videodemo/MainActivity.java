2cad932fc00aebff3ac693efeed8e144604a8ebe
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-08-19 10:28:57 -0700

    video plays with controls

--- app/src/main/java/com/andrewflincoln/videodemo/MainActivity.java
+++ app/src/main/java/com/andrewflincoln/videodemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.andrewflincoln.videodemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mexicola_basement);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
+
+
     }
 }
